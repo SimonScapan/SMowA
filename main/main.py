135ad77d19a32e1ce196da439a1dd769d9cb4654
SimonScapan <s-scapan@web.de>
2023-06-27 19:46:50 +0200

    continuous testing

--- main/main.py
+++ main/main.py
@@ -95,6 +95,6 @@ def destroy():
 
 if __name__ == '__main__':
     try:
-    app.run(host='0.0.0.0', debug=False)
+        app.run(host='0.0.0.0', debug=False)
     except KeyboardInterrupt:
         destroy()
