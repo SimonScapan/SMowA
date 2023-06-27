ebe27144486670f2be8b03c01c93a9b7f52c16ac
SimonScapan <s-scapan@web.de>
2023-06-27 19:21:46 +0200

    check camera tilt settings

--- main/main.py
+++ main/main.py
@@ -38,9 +38,7 @@ pan_servo.offset = 10
 tilt_servo.offset = 0
 
 fw.turn(100)
-#pan_servo.write(90)
-tilt_servo.write(60)
-motor_speed = 60
+tilt_servo.write(80)
 
 #################################
 # initialize Monitoring Website #
