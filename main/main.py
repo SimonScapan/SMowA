f740f422c805a2418558dc58da64cd4288d74f37
SimonScapan <s-scapan@web.de>
2023-06-27 19:42:00 +0200

    check camera tilt settings

--- main/main.py
+++ main/main.py
@@ -96,5 +96,5 @@ def destroy():
 if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', debug=False)
-    except KeyboardInterrupt:
-        destroy()
+    #except KeyboardInterrupt:
+    #    destroy()
