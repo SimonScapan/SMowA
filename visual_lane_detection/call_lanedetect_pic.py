f08043d2eaa04dde48eb4cceef649cc190180a99
SimonScapan <s-scapan@web.de>
2023-07-03 12:41:30 +0200

    change for project worklecy

--- visual_lane_detection/call_lanedetect_pic.py
+++ visual_lane_detection/call_lanedetect_pic.py
@@ -2,7 +2,7 @@
 #
 # for testing purposes you can run this script with Data from the Drive
 # 
-# change wether you want indoor or outdoor data in line 17
+# change wether you want indoor or outdoor data in line 22
 #
 # the output shows the detected lane with the steering angle value (minus values for turning right and vise versa)
 #
@@ -19,16 +19,16 @@ from timeit import default_timer as timer
 import numpy as np
 
 # set indoor or outdoor depending on test environment
-location = 'outdoor'
+location = 'indoor'
 
 time = []
 
-for image_path in list(os.listdir(f'./lane_detection_data/{location}')):
+for image_path in list(os.listdir(f'./visual_lane_detection/lane_detection_data/{location}')):
     # start timer for speed calculation of lane detection
     start = timer()
 
     # call lane detection for all pictures in folder
-    image = mpimg.imread(f'./lane_detection_data/{location}/{image_path}') 
+    image = mpimg.imread(f'./visual_lane_detection/lane_detection_data/{location}/{image_path}')
     try:
         picture, canny, steering = lanedetect_steer.lane_detection(image, location)
     except Exception:
@@ -41,10 +41,10 @@ for image_path in list(os.listdir(f'./lane_detection_data/{location}')):
     # plot outputs
     fig = plt.figure(figsize=(20, 10),num=f'{image_path}')
     ax = fig.add_subplot(1, 2, 1,xticks=[], yticks=[])
-    plt.imshow(canny)
+    plt.imshow(picture)
     ax.set_title("canny transformation")
     ax = fig.add_subplot(1, 2, 2,xticks=[], yticks=[])
-    plt.imshow(picture)
+    plt.imshow(canny)
     ax.set_title(f"Output Image - Steering: {steering}") 
     plt.show()
 
