2e23aa69739fb1f824434f8ea8dab76f4852f017
SimonScapan <s-scapan@web.de>
2023-07-20 15:29:07 +0200

    test

--- main/lane_detection/lanedetect_steer.py
+++ main/lane_detection/lanedetect_steer.py
@@ -262,7 +262,7 @@ def lane_finding_pipeline_indoor(image):
     # Change Brightness and Contrast to avoid misclassification caused by ground   
     bc_img = brightness_contrast(input_img = gray_img, contrast = 2, brightness = 0.4)
     # Gaussian Smoothing
-    smoothed_img = gaussian_blur(img = bc_img, kernel_size = 21)
+    smoothed_img = gaussian_blur(img = bc_img, kernel_size = 13)
     
     ## Canny Edge Detection
     # Calculate good threshold
