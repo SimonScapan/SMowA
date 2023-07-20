7b0a1bc42d9bd12e71ba9a652df36ecaee3e647d
SimonScapan <s-scapan@web.de>
2023-07-20 09:34:17 +0200

    changes for on Prem test

--- main/lane_detection/lanedetect_steer.py
+++ main/lane_detection/lanedetect_steer.py
@@ -287,7 +287,7 @@ def lane_finding_pipeline_indoor(image):
     # compute steering advice for car
     steering = steer(image, left_line, right_line)
 
-    output = bc_img
+    #output = gray_img
 
     return output, canny_mask, steering
 
