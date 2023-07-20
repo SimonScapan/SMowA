325ceabad4439105d5c1209a2281bdf7e17ca08f
SimonScapan <s-scapan@web.de>
2023-07-20 13:51:45 +0200

    test

--- main/lane_detection/lanedetect_steer.py
+++ main/lane_detection/lanedetect_steer.py
@@ -286,7 +286,7 @@ def lane_finding_pipeline_indoor(image):
     # compute steering advice for car
     steering = steer(image, left_line, right_line)
 
-    output = masked_img
+    output = canny_img
 
     return output, canny_mask, steering
 
