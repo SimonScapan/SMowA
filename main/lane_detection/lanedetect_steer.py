53b8750bf120448d3a1089a3ec88a17357a2ef03
SimonScapan <s-scapan@web.de>
2023-07-20 13:40:51 +0200

    test

--- main/lane_detection/lanedetect_steer.py
+++ main/lane_detection/lanedetect_steer.py
@@ -259,7 +259,7 @@ def lane_finding_pipeline_indoor(image):
     # Grayscale
     gray_img = grayscale(image)
     # Change Brightness and Contrast to avoid misclassification caused by ground   
-    bc_img = brightness_contrast(input_img = gray_img, contrast = 3, brightness = 0.5)
+    bc_img = brightness_contrast(input_img = gray_img, contrast = 1.8, brightness = 0.7)
     # Gaussian Smoothing
     smoothed_img = gaussian_blur(img = bc_img, kernel_size = 17)
     
