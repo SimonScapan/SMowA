dcba3fb806fbefc719aad02b549d86f5d58aeddd
SimonScapan <s-scapan@web.de>
2023-07-21 09:06:38 +0200

    last day testing

--- main/lane_detection/lanedetect_steer.py
+++ main/lane_detection/lanedetect_steer.py
@@ -94,7 +94,7 @@ def get_vertices(image, scope):
 
     if scope == 'border':
         rows, cols = image.shape[:2]
-        bottom_left  = [cols*1.2, rows]
+        bottom_left  = [cols*0, rows]
         top_left     = [cols*0.1, rows*0.4]
         bottom_right = [cols*1.2, rows]
         top_right    = [cols*0.9, rows*0.4] 
